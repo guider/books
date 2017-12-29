f2e6dadb3ef791a2335d8b72cd73846464a339ef
guider <guider@yeah.net>
2017-12-29 16:17:49 +0800

    fix

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -112,13 +112,13 @@ module.exports = {
                 include: paths.appSrc,
                 loader: require.resolve('babel-loader'),
                 options: {
-                    // 改动: 添加 antd 按需加载文件处理插件
+                    // 改动: 添加 antd-mobile 按需加载文件处理插件
                     plugins: [
                         //['react-html-attrs'],//添加babel-plugin-react-html-attrs组件的插件配置
                         // 引入样式为 css
                         ['import', {libraryName: 'antd-mobile', style: 'css'}],
                         // 改动: 引入样式为 less
-                        //  ['import', { libraryName: 'antd', style: true }],
+                        //  ['import', { libraryName: 'antd-mobile', style: true }],
                     ],
                     // This is a feature of `babel-loader` for webpack (not Babel itself).
                     // It enables caching results in ./node_modules/.cache/babel-loader/
