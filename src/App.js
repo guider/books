ca77c2b4ff45bfdd5711861db3d3bb5f5b676dca
guider <guider@yeah.net>
2017-12-29 16:44:15 +0800

    add poster

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.css';
-import {Button} from 'antd-mobile'
 import CSSModules from 'react-css-modules'
+
+import {BrowserRouter,Link,Route,Prompt,NavLink,Redirect} from 'react-router-dom'
+
+import MainPage from './pages/main/MainPage'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p styleName="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Button> btn</Button>
+      <div styleName="App">
+
+          <MainPage />
+
+
+        {/*<header className="App-header">*/}
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<h1 className="App-title">Welcome to React</h1>*/}
+        {/*</header>*/}
+        {/*<p styleName="App-intro">*/}
+          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+          {/*<Button> btn</Button>*/}
       </div>
     );
   }
---
+++ src/pages/main/MainPage.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class MainPage extends Component {
+
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <div> new template </div>
+        )
+    }
+    componentDidMount(){
+
+    }
+}
+
+export default MainPage
